cd393afac092057d794621041ce435247b09d7d6
Ngocdzung Smith <hysinh1@gmail.com>
2024-06-27 12:05:01 +0000

    Create taskmanager database

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-if os.path.exists("env.py"): 
+if os.path.exists("env.py"):  #in order to use any of the hidden environment variables, import "env" package
     import env  # noqa
 
 
@@ -11,4 +11,5 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app) 
 
+#imported last bc "routes" file will rely on using both the "app" and "db" variables definted about
 from taskmanager import routes  # noqa
