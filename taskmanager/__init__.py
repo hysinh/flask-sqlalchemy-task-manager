7442832d6b698ffaf5c297933d59ade528383c55
Ngocdzung Smith <hysinh1@gmail.com>
2024-06-29 10:00:39 +0000

    Adds add category form functionality

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -2,14 +2,14 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):  #in order to use any of the hidden environment variables, import "env" package
-    import env  # noqa
+    import env  # noqa  # noqa
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
-db = SQLAlchemy(app) 
+db = SQLAlchemy(app)
 
 #imported last bc "routes" file will rely on using both the "app" and "db" variables definted about
 from taskmanager import routes  # noqa
